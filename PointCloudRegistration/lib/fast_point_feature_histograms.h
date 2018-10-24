6db920d39001916bab8ae3131592f0883b82c63b
TimFelle <timfelle@hotmail.com>
2018-10-24 13:06:03 +0200

    Implementation of FPFH should be done now.
    The selection of correspondances is still missing

--- PointCloudRegistration/lib/fast_point_feature_histograms.h
+++ PointCloudRegistration/lib/fast_point_feature_histograms.h
@@ -4,10 +4,11 @@
 // Includes
 #include <string>
 #include <vector>
-#include <Core/Core.h>	// Open3D
+#include <Open3D/Core/Core.h>
+#include <Eigen/Dense>
 // ============================================================================
 // Function prototypes
-std::vector<Eigen::Vector2i> computeCorrespondancePair(open3d::PointCloud model_0, open3d::PointCloud model_1);
-std::vector<Eigen::Vector3f> computeFPFH(open3d::PointCloud model);
+std::vector<Eigen::Vector2i> computeCorrespondancePair(open3d::PointCloud &model_0, open3d::PointCloud &model_1);
+void computeFPFH(open3d::PointCloud &model, std::vector<int> &P_0, std::vector<Eigen::VectorXd> &FPFH);
 
 #endif	/* __FAST_POINT_FEATURE_HISTOGRAMS__ */
