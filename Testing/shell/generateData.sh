3c397eae4f9aba4336dcd2106b77fcdcd8883636
TimFelle <timfelle@hotmail.com>
2018-10-31 14:18:54 +0100

    Added test to visualise GenerateData capabilities

---
+++ Testing/shell/generateData.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+echo "====================================================================="
+echo "generateData.sh:"
+echo "   This is the test generateData. This test will display the effect"
+echo "   of several settings for the data generation program."
+
+DAT=../../data/
+FIG=../../figures/
+MAT=../../matlab
+
+# ==============================================================================
+# Generate all the data needed
+
+export INPUT_PATH=$DAT
+export OUTPUT_PATH=./
+
+# Clean model
+cp $DAT/bunnyPartial1.ply bunnyClean.ply
+MODEL="'bunnyClean', "
+
+# Test the Gaussian noise
+export NOISE_TYPE=gaussian
+export NOISE_STRENGTH=1.5
+./GenerateData bunnyPartial1.ply bunnyGaussian.ply > loadingBar.out
+
+MODEL+="'bunnyGaussian', "
+
+# Test the Outlier noise
+export NOISE_TYPE=outliers
+export OUTLIER_AMOUNT=5.0
+./GenerateData bunnyPartial1.ply bunnyOutliers.ply > loadingBar.out
+
+MODEL+="'bunnyOutliers', "
+
+# Test combination noise
+export NOISE_TYPE=both
+export NOISE_STRENGTH=2.0
+export OUTLIER_AMOUNT=5.0
+./GenerateData bunnyPartial1.ply bunnyNoise.ply > loadingBar.out
+
+MODEL+="'bunnyNoise', "
+
+# Test transformation
+export NOISE_TYPE=none
+export ROTATION="0.52,0.52,0.79" # degrees: 30, 30, 45
+export TRANSLATION="0.0,0.0,0.0"
+./GenerateData bunnyPartial1.ply bunnyTransform.ply > loadingBar.out
+
+MODEL+="'bunnyTransform' "
+
+rm -fr loadingBar.out # Remove all the loading bars again.
+# ==============================================================================
+# Export the figures using matlab
+mkdir fig
+matlab -r "addpath('$MAT');displayModel({$MODEL},'./','fig');exit;" -wait -nodesktop -nosplash
+mv -t $FIG fig/*
