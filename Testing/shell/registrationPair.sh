c02a8d76e2a50fb24be2ab923531f82a5cce78c6
TimFelle <timfelle@hotmail.com>
2018-11-08 10:22:13 +0100

    Added test for registration

--- Testing/shell/registrationPair.sh
+++ Testing/shell/registrationPair.sh
@@ -11,7 +11,7 @@ MAT=../../matlab
 
 # ==============================================================================
 # Generate all the data needed
-export INPUT_PATH=$DAT
+export INPUT_PATH=./
 export OUTPUT_PATH=./
 
 echo "Input and output paths defined by:                                   "
@@ -27,39 +27,18 @@ MODEL="'bunnyClean', "
 echo "====================================================================="
 echo "Commencing tests:                                                    "
 
-# Test the Gaussian noise
-echo "   Gaussian noise.                                                   "
-export NOISE_TYPE=gaussian
-export NOISE_STRENGTH=1.5
-./GenerateData bunnyPartial1.ply bunnyGaussian.ply > loadingBar.out
-
-MODEL+="'bunnyGaussian', "
-
-# Test the Outlier noise
-echo "   Outlier addition.                                                 "
-export NOISE_TYPE=outliers
-export OUTLIER_AMOUNT=5.0
-./GenerateData bunnyPartial1.ply bunnyOutliers.ply > loadingBar.out
-
-MODEL+="'bunnyOutliers', "
-
-# Test combination noise
-echo "   Combined noise and outlier.                                       "
-export NOISE_TYPE=both
-export NOISE_STRENGTH=2.0
-export OUTLIER_AMOUNT=5.0
-./GenerateData bunnyPartial1.ply bunnyNoise.ply > loadingBar.out
-
-MODEL+="'bunnyNoise', "
-
 # Test transformation
 echo "   Transformation.                                                   "
 export NOISE_TYPE=none
 export ROTATION="0.52,0.52,0.79" # degrees: 30, 30, 45
 export TRANSLATION="0.0,0.0,0.0"
-./GenerateData bunnyPartial1.ply bunnyTransform.ply > loadingBar.out
+./GenerateData bunnyClean.ply bunnyTransform.ply > loadingBar.out
 
 MODEL+="'bunnyTransform' "
+
+# Test registration
+
+./Registration bunnyClean.ply bunnyTransform.ply
 
 rm -fr loadingBar.out # Remove all the loading bars again.
 # ==============================================================================
@@ -67,7 +46,7 @@ rm -fr loadingBar.out # Remove all the loading bars again.
 echo "Running matlab to complete visualisation.                            "
 mkdir fig
 matlab -wait -nodesktop -nosplash \
-	-r "addpath('$MAT');displayModel({$MODEL},'./','fig');exit;" 
+	-r "addpath('$MAT');displayRegistration({$MODEL},'./','fig');exit;" 
 mv -t $FIG fig/*
 echo "Results placed in folder:                                            "
 echo $FIG
