50f4e168ec8978cc4c8834c830b62a723954b04a
timfelle <timfelle@hotmail.com>
2018-12-18 10:07:06 +0100

    Added submitted tests to the LSF10 system

---
+++ Testing/submit.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+# submit.sh TESTNAME [TESTNAME ...]
+#
+# This script works as a function which run tests for the executables
+# implementated in the PointCloudRegistration folder of the project.
+# 
+# The <TESTNAME> variable refers to the filename of the TESTNAME.sh files
+# located in the folder shell. These TESTNAME.sh files contain the 
+# definition of the test which should be run.
+
+# Submit function
+submit(){
+	if [ ! -s error.err ]; then
+	 	echo "No error"
+	else
+		echo "We have error"
+	fi
+}
+
+# Help commands
+if  [ "$1" = "-h" ] || [ "$1" = "--help" ] ; then
+	echo "submit.sh TESTNAME [TESTNAME ...]"
+	echo "This function run tests for the executables implemented in the"
+	echo "PointCloudRegistration folder of the project."
+	echo ""
+	echo "The <TESTNAME> variable refers to the filename of the TESTNAME.sh"
+	echo "files located in the folder shell. These TESTNAME.sh files contain "
+	echo "the definition of the test which should be run."
+	exit
+fi
+
+
+# Define all needed folders relative to the Testing folder.
+EPATH=../PointCloudRegistration
+DPATH=data
+FPATH=figures
+LPATH=logs
+SPATH=submit
+
+# Check for test specifications
+if [ -z "$1" ] ; then
+	>&2 echo "================================================================="
+	>&2 echo "| ERROR in submit.sh: (No test)                                 |"
+	>&2 echo "|    Please specify which test to run.                          |"
+	>&2 echo "|    Jobs are specified by the filename only.                   |"
+	>&2 echo "|    Possible files can be seen in \"$SPATH\":                  |"
+	>&2 echo "|                                                               |"
+	>&2 ls $SPATH
+	>&2 echo "================================================================="
+	exit
+else
+	TEST="$@"
+fi
+
+# Make sure the excecutables exists
+cd $EPATH;
+if [ ! -f GenerateData.exe ] || [ ! -f Registration.exe ] ; then
+	echo "Executables not found, attempting make."
+	make -s 
+fi
+if [ ! -f GenerateData.exe ] ; then
+	>&2 echo "================================================================="
+	>&2 echo "| ERROR in submit.sh: (Missing executable)                      |"
+	>&2 echo "|    GenerateData.exe not found. Aborting tests.                |"
+	>&2 echo "|    Please attempt a manual compilation.                       |"
+	>&2 echo "================================================================="
+	exit
+fi
+if [ ! -f Registration.exe ] ; then
+	>&2 echo "================================================================="
+	>&2 echo "| ERROR in submit.sh: (Missing executable)                      |"
+	>&2 echo "|    Registration.exe not found. Aborting tests.                |"
+	>&2 echo "|    Please attempt a manual compilation.                       |"
+	>&2 echo "================================================================="
+	exit
+fi
+cd ../Testing
+
+# Define files needed by the execution in all tests
+EXEC="$EPATH/Registration.exe $EPATH/GenerateData.exe"
+
+# Create all needed folders
+mkdir -p $DPATH $FPATH $LPATH
+
+# Run the tests
+set -e
+echo " "
+echo "------------------------------------------------------------------"
+echo "Queueing tests: "; echo " "
+for test in $TEST
+do
+	echo "  $test."
+	if [ -f "$SPATH/$test.sh" ] ; then
+
+		# Create the folder needed
+		rm -fr $LPATH/submit$test
+		mkdir -p $LPATH/submit$test
+
+		# Copy all files needed
+		cp -t $LPATH/submit$test -f $SPATH/$test.sh $EXEC
+
+		# Move to the directory submit the code and return
+		cd $LPATH/submit$test
+		bsub < $test.sh
+		cd ../../
+	else
+		>&2 echo "File $SPATH/$test.sh was not found"
+	fi
+done
+echo " "
+echo "------------------------------------------------------------------"
+echo "Tests submited."
+# # EOF # #
---
+++ Testing/submit/correspondences.sh
@@ -0,0 +1,172 @@
+#!/bin/sh 
+
+# --  General options 
+
+# Naming of the job and queue name
+#BSUB -J correspondences
+#BSUB -q hpc
+
+# Specify
+#BSUB -oo output.out 
+#BSUB -eo error.err 
+
+# -- Technical options
+
+# Ask for n cores placed on R host.
+#BSUB -n 1
+#BSUB -R "span[ptile=1]"
+
+# Memory specifications. Amount we need and when to kill the
+# program using too much memory.
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -M 5GB
+
+# Time specifications (hh:mm)
+#BSUB -W 00:30
+
+# -- Notification options
+
+# Set the email to recieve to and when to recieve it
+##BSUB -u your_email_address
+#BSUB -B		# Send notification at start
+#BSUB -N 		# Send notification at completion
+
+echo --------------------------------------------------------------------------
+echo 'Job: '$LSB_JOBNAME', is running on '$LSB_DJOB_NUMPROC' cores.'
+echo --------------------------------------------------------------------------
+echo LSB: job identifier is $LSB_JOBID
+echo LSB: execution queue is $LSB_QUEUE
+echo LSB: total number of processors is $LSB_MAX_NUM_PROCESSORS
+echo LSB: working directory is $LSB_OUTDIR
+echo --------------------------------------------------------------------------
+
+# End of LSB info
+#==============================================================================
+# Define Preparation
+
+DAT=../../data
+FIG=../../figures/Correspondences
+MAT=../../matlab
+
+export INPUT_PATH="$DAT"
+export OUTPUT_PATH="dat/"
+
+Prepare()
+{
+	echo ' '
+	echo Preparing
+	mkdir fig $FIG -p
+	lscpu >> $LSB_JOBNAME.cpu
+
+}
+
+# End of Preparation
+#==============================================================================
+# Define Program
+
+Program()
+{
+	echo ' '
+	echo Running computations
+
+	start=`date +%s`
+	# -------------------------------------------------------------------------
+	# Define the actual test part of the script 
+		
+	echo "Input and output paths defined by:                                   "
+	echo "Input : $INPUT_PATH                                                  "
+	echo "Output: $OUTPUT_PATH                                                 "
+	echo "                                                                     "
+
+	# Clean model
+	echo "   Fetching clean models"
+	cp $DAT/bunnyPartial1.ply bunnyClean.ply
+	cp $DAT/bunnyPartial2.ply bunnyTransform.ply
+
+	# Test transformation
+	echo "   Generating transformed model."
+	export NOISE_TYPE=none
+	export ROTATION="0.52,0.52,0.79" # degrees: 30, 30, 45
+	export TRANSLATION="0.0,0.0,0.0"
+	./GenerateData.exe bunnyTransform.ply bunnyTransform.ply
+
+	echo "====================================================================="
+	echo "Commencing tests:                                                    "
+
+	# Test registration
+	export EXPORT_CORRESPONDENCES="true"
+	./Registration.exe bunnyClean.ply bunnyTransform.ply
+
+	if [ -s error.err ] ; then
+		exit
+	fi
+	# -------------------------------------------------------------------------
+	end=`date +%s`
+
+	runtime=$((end-start))
+	echo "Time spent on computations: $runtime"
+}
+
+# End of Program
+#==============================================================================
+# Define Visualize
+
+Visualize()
+{
+	echo ' '
+	echo Visualizing
+	matlab -r "addpath('$MAT');animateCorrespondences('Corr','./','fig');exit;"
+}
+
+# End of Visualize
+#==============================================================================
+# Define Finalize
+
+Finalize()
+{
+	echo ' '
+	echo Finalizing
+
+	mv -ft $FIG fig/*
+	rm -fr *.ply *.exe *.sh fig dat
+
+	echo Figures moved to $FIG.
+	echo Test concluded successfully.
+}
+
+# End of Visualize
+#==============================================================================
+# Define Early Termination
+
+early()
+{
+	echo ' '
+	echo ' ================= WARNING: EARLY TERMINATION ================= '
+	echo ' '
+}
+trap 'early' 2 9 15
+
+# End of Early Termination
+#==============================================================================
+# Call Functions
+
+Prepare
+
+echo ' '
+echo --------------------------------------------------------------------------
+
+Program
+
+echo ' '
+echo --------------------------------------------------------------------------
+
+Visualize
+
+echo ' '
+echo --------------------------------------------------------------------------
+
+Finalize
+
+echo ' '
+echo --------------------------------------------------------------------------
+# ==============================   End of File   ==============================
