01f0418082e212d33311d7a44950d3f62225d6a1
TimFelle <timfelle@hotmail.com>
2019-02-07 12:46:55 +0100

    Test updates

---
+++ Testing/shell/iterationTest.sh
@@ -0,0 +1,140 @@
+#!/bin/sh
+#==============================================================================
+# Define Preparation of data ect.
+
+Prepare()
+{
+	# Setup folders needed
+	TNAME=`basename -- "$0"`
+	FIG=../../figures/${TNAME%.*}
+	DAT=../../data
+	MAT=../../matlab
+
+	mkdir -p fig 
+
+	# Define input and output paths
+	export INPUT_PATH="dat/"
+	export OUTPUT_PATH="dat/"
+
+	echo "Input and output paths defined by:"
+	echo "   Input : $INPUT_PATH"
+	echo "   Output: $OUTPUT_PATH"
+	echo ' '
+
+	mkdir -p dat
+	cp -ft dat/ $DAT/seal_*
+
+	ROTATION=1,0,0 \
+	./GenerateData seal_left.ply seal_left.ply
+	TRANSLATION=0,-600,-200 \
+	./GenerateData seal_right.ply seal_right.ply
+}
+
+# End of Preparation
+#==============================================================================
+# Define the actual test part of the script 
+
+Program()
+{
+	SETS="'seal'"
+	I="1 5 10 20 50 70 100 150 300 1000"
+	for i in $I ; do
+		SETS+=",'res$i-'"
+		FGR_MAXITER=$i \
+		OUTPUT_NAME="res$i-" \
+		FGR_VERSION=open3d \
+		./Registration.exe seal
+	done
+}
+
+# End of Program
+#==============================================================================
+# Define Visualize
+
+Visualize()
+{
+	echo ' '
+	echo Visualizing
+	MATOPT="-wait -nodesktop -nosplash"
+	matlab $MATOPT \
+	-r "addpath('$MAT');displayRegistration({$SETS},'dat','fig');exit;"
+	matlab $MATOPT \
+	-r "addpath('$MAT');animateRegProcess({$SETS},'dat','fig');exit;"
+}
+
+# End of Visualize
+#==============================================================================
+# Define Finalize
+
+Finalize()
+{
+	rm -fr $FIG
+	mkdir -p fig $FIG $FIG/data 
+
+	mv -ft $FIG fig/* 2>/dev/null
+	mv -ft $FIG/data dat/* 2>/dev/null
+	rm -fr *.exe *.sh fig dat
+
+	echo "   Figures moved to $FIG."
+	echo "   Data used located in $FIG/data"
+}
+
+# End of Visualize
+#==============================================================================
+# Define Early Termination
+
+Early()
+{
+	if [ -s error.err ] ; then
+		rm -fr *.ply *.exe *.sh fig dat
+		echo ' '
+		echo ' ================= WARNING: EARLY TERMINATION ================= '
+		cat error.err 
+		echo ' ===================== ERRORS SHOWN ABOVE ===================== '
+		echo ' '
+		exit 2
+	fi
+}
+
+# End of Early Termination
+#==============================================================================
+# Call Functions
+echo __________________________________________________________________________
+echo 'Preparing data and folders'
+echo ' '
+full_start=`date +%s.%N`
+
+Early && Prepare
+
+echo ' '
+echo __________________________________________________________________________
+echo 'Commencing tests:'
+echo ' '
+test_start=`date +%s.%N`
+Early && Program
+
+test_end=`date +%s.%N`
+runtime=$(echo $test_end $test_start | awk '{ printf "%f", $1 - $2 }')
+echo "Computation time for test: $runtime seconds."
+
+
+
+echo ' '
+echo __________________________________________________________________________
+
+Early && Visualize
+
+echo ' '
+echo __________________________________________________________________________
+echo ' '
+echo Finalizing
+
+Early && Finalize
+
+Early
+full_end=`date +%s.%N`
+runtime=$(echo $full_end $full_start | awk '{ printf "%f", $1 - $2 }')
+echo "Computation time for full test: $runtime seconds."
+echo 'Test concluded successfully.'
+echo __________________________________________________________________________
+# ==============================   End of File   ==============================
